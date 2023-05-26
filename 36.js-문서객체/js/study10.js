1ba02c63234c0f86dbf48f80e17a365e4b947d2c
Mona Lisa <true4alse@naver.com>
2023-05-26 11:19:50 +0900

    pearl

--- 36.js-문서객체/js/study10.js
+++ 36.js-문서객체/js/study10.js
@@ -3,8 +3,8 @@ window.onload = function(){
     let station1 = new Swiper(".station1",{
         loop:true,
         direction:"horizontal",
-        slidesPerView:3.2, //슬라이드 개수의 반만큼 최대 적용 가능
-        spaceBetween:40,
+        // slidesPerView:3.2, //슬라이드 개수의 반만큼 최대 적용 가능
+        // spaceBetween:40,
         pagination: {
             el: '.swiper-pagination',
             type: 'bullets',
@@ -14,7 +14,32 @@ window.onload = function(){
             nextEl: ".csNextBtn",
             prevEl: ".csPrevBtn",
         },
-        autoplay:true
+        autoplay:{
+            delay:1000,
+            // pauseOnMouseEnter:true,
+        },
+        speed:1000,
+        breakpoints:{
+            320:{
+                slidesPerView:1,
+                spaceBetween:0
+            },
+            768:{
+                slidesPerView:2,
+                spaceBetween:10
+            },
+            1024:{
+                slidesPerView:3,
+                spaceBetween:20
+            }
+        }
+    })
+
+    document.querySelector(".station1").addEventListener("mouseover",function(){
+        station1.autoplay.stop()
+    })
+    document.querySelector(".station1").addEventListener("mouseout",function(){
+        station1.autoplay.start()
     })
    
     
