e0cc58ce38f8bb788ed9c98a249339091de1a6d1
Mona Lisa <true4alse@naver.com>
2023-05-30 21:52:56 +0900

    test

--- pearlabyss-ex/js/common.js
+++ pearlabyss-ex/js/common.js
@@ -1,6 +1,21 @@
 window.onload = function(){
 
-    let newsSlider = new Swiper(".news_station",{});
+    let newsSlider = new Swiper(".news_station",{
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        breakpoints:{
+            280:{
+                slidesPerView : 1
+            },
+            768:{
+                slidesPerView : 3
+            }
+        }
+    });
 
 
 }
+
+
+
