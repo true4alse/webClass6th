6d630c47195588b42a8e7a17f77d6a273ef923a6
Mona Lisa <true4alse@naver.com>
2023-06-08 14:38:54 +0900

    wheel event

---
+++ 38.js-제이쿼리/js/study6.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        let winst = $(window).scrollTop()
+        let con2top = $(".con2").offset().top
+        if(winst>=con2top){
+            $(".con2").addClass("on")
+        }else{
+            $(".con2").removeClass("on")
+        }
+    })
+
+    $(".wrap>div").on("wheel DOMMouseScroll",function(event){
+    //    console.log(event)
+       let E = event.originalEvent
+       let delta = 0;
+       if(E.detail){
+        delta = E.detail * -40
+       }else{
+        delta = E.wheelDelta
+       }
+       
+       if(delta<0){
+        //마우스 휠을 내렸을 때
+        if($(this).next().length!=0){
+            let posTop = $(this).next().offset().top
+            $("html,body").stop().animate({scrollTop:posTop},1000)
+        }
+       }else{
+        //마우스 휠을 올렸을 때
+        if($(this).prev().length!=0){
+            let posTop = $(this).prev().offset().top
+            $("html,body").stop().animate({scrollTop:posTop},1000)
+        }
+       }
+       return false
+
+    })
+    
+
+
+})
