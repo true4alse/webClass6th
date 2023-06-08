6bde4dbd60ba2e9b508a5297ab397290a37031d0
Mona Lisa <true4alse@naver.com>
2023-06-08 11:31:56 +0900

    screoll

--- 38.js-제이쿼리/js/study4.js
+++ 38.js-제이쿼리/js/study4.js
@@ -4,12 +4,18 @@ $(document).ready(function(){
     $(window).scroll(function(){
        let winSCT = $(window).scrollTop()
        console.log(winSCT)
-       if(winSCT>400){
-            // alert("400이상 아래로 스크롤해서 내려왔습니다.")
-            $("body").addClass("dark")
+     //   if(winSCT>400){
+     //        // alert("400이상 아래로 스크롤해서 내려왔습니다.")
+     //        $("body").addClass("dark")
+     //   }else{
+     //        $("body").removeClass("dark")
+     //   }
+       if(winSCT>400 && winSCT<1500){
+          $("body").css("background","red")
        }else{
-            $("body").removeClass("dark")
+          $("body").css("background","transparent")
        }
+
     })
 
 })
--- 38.js-제이쿼리/js/study5.js
+++ 38.js-제이쿼리/js/study5.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
+    
+    $(".box").animate({width:400},1000)
+    $(".box").animate({height:400},1000)
+    $(".box").animate({marginLeft:200},1000)
+    $(".box").clearQueue()
+    
+    
+    
 
-    // $(".box").animate({width:400,height:400},10000)
+    // $(".box").animate({width:400},1000).animate({height:400},1000).animate({marginLeft:200},1000)
+
     // $(".box").animate({width:400},1000).animate({height:400},1000).clearQueue()
     // $(".box").animate({width:400},1000).animate({height:400},1000).stop()
     // $(".box").delay(1000).animate({width:400},1000).animate({height:400},1000)
 
     let b1top = $(".b1").offset().top
-    let b2top = $(".b2").offset().top
-    $("html,body").animate({scrollTop:b2top},500)
+    console.log(b1top)
+    // let b2top = $(".b2").offset().top
+    // $("html,body").animate({scrollTop:b2top},500)
 
     
 });
