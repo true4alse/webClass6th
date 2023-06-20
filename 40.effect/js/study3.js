8e88824022ad287916a76f3186281b579427ff7a
Mona Lisa <true4alse@naver.com>
2023-06-20 13:22:29 +0900

    textani

--- 40.effect/js/study3.js
+++ 40.effect/js/study3.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
     
-
-    $(".btn").click(function(){
-        // textAni(".title",{
-        //     aniDelay:0.5,
-        //     aniSpeed:1,
-        //     textSize:80
-        // })
-        textAni(".title",{
-            textSize:100,
-            aniSpeed:0.5
-        })
+    
+    textAni(".title",{
+        textSize:100,
+        aniSpeed:0.5,
+        effect:"fadeInOut"
     })
+    textAni(".title2",{
+        textSize:30,
+        aniSpeed:0.5,
+        aniDelay:0.01,
+        effect:"moveTopToBottom"
+    })
+
+
 
 
 
--- 40.effect/js/textAniamtion.js
+++ 40.effect/js/textAniamtion.js
@@ -3,15 +3,24 @@ function textAni(cssSelector,option){
     // aniDelay 글자들이 나타나는 간격
     // aniSpeed 글자들이 나타나는 속도
     // textSize 글자들에게 적용하는 크기
+    let lettersLength = $(cssSelector).get(0).innerText.length
+    let result =""
+    for(let i=0 ; i<lettersLength ; i++){
+        result+="<b>"+$(cssSelector).get(0).innerText[i]+"</b>"
+    }
+    $(cssSelector).html(result)
+    // 제이쿼리문서객체.get(0) => 실행하게되면 제이쿼리문서객체를 자바스크립트문서객체로 변환하여 리턴한다.
+    // 문자데이터들은 배열처럼 구성이 되어있다.
 
     let aniamtionTerm = option.aniDelay?option.aniDelay:0.5
     let animationDuration = option.aniSpeed?option.aniSpeed:1
     let aniTxtSize = option.textSize?option.textSize:20
+    let aniName = option.effect?option.effect:"introAni"
 
     $(cssSelector).css("font-size","0")
     $(cssSelector+" b").css("opacity","0").css("line-height","1").css("display","inline-block").css("font-size",aniTxtSize+"px")
 
     for(let i=0 ; i<$(cssSelector+" b").length ; i++){
-        $(cssSelector+" b:nth-of-type("+(i+1)+")").css("animation","introAni "+animationDuration+"s ease "+(aniamtionTerm*i)+"s forwards")
+        $(cssSelector+" b:nth-of-type("+(i+1)+")").css("animation",aniName+" "+animationDuration+"s ease "+(aniamtionTerm*i)+"s forwards")
     }   
 }
