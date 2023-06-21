550293e05531e5603e86bef541838b84aa86f1e8
Mona Lisa <true4alse@naver.com>
2023-06-21 16:43:57 +0900

    mousemove

--- 40.effect/js/study4.js
+++ 40.effect/js/study4.js
@@ -12,10 +12,56 @@ $(document).ready(function(){
     }
     $(".train").html(result)
 
+    $(".stationMenu>li").click(function(){
+        let idx = $(this).index() // 클릭한 리스트의 순번을 리턴함.
+        count = idx*5 // 정해진 순번에 5를 곱한 값을 전역변수 count에 저장함으로써 휠을 내렸을 때 이동되는 기능이 정상적으로 작동되게끔 해주는 코드
+        // alert(idx)
+        // 01 -> 0px
+        // 06 -> -1000px
+        // 11 -> -2000px
+        // 16 -> -3000px
+        $(".train").css("transform","translateX("+(-1000*idx)+"px)")
+    })
+
+    let count=0;
+    $(".station").on("wheel DOMMouseScroll",function(event){
+        let E = event.originalEvent
+        let delta = 0;
+        if(E.detail){
+        delta = E.detail * -40
+        }else{
+        delta = E.wheelDelta
+        }
+        
+        if(delta<0){
+        //마우스 휠을 내렸을 때
+            count++
+            console.log(count)
+            if(count>15){count=15}
+            $(".train").css("transform","translateX("+(-200*count)+"px)")
+        }else{
+        //마우스 휠을 올렸을 때
+            count--
+            if(count<0){count=0}
+            $(".train").css("transform","translateX("+(-200*count)+"px)")
+        }
+        return false
+    })
+
+    // 클릭한 리스트(기차칸)의 활성화 스타일 기능
+    $(".train>li").click(function(){
+        $(".train>li").removeClass("on")
+        $(this).addClass("on")
+        let idx = $(this).index()
+        count = idx //동기화시킴
+        $(".train").css("transform","translateX("+(-200*idx)+"px)")
+    })
+
+    //닫는버튼 기능
+    $(".btnClose").click(function(){
+        $(".train>li").removeClass("on")
+        return false //상위태그로 클릭이벤트가 전달되지않게(버블링되지않게) 설정
+    })
+
 })
 
-// let a = 4
-// a=a+2
-// a+=2
-// b=b-2
-// b-=2
