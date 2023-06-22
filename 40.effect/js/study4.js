cf56f6c47ca10bb45b39f56759ffc9e6a2d68fec
Mona Lisa <true4alse@naver.com>
2023-06-22 10:37:43 +0900

    Array

--- 40.effect/js/study4.js
+++ 40.effect/js/study4.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
 
     let result=""
-    for(let i=1 ; i<=20 ; i++){
-        result+=`<li>
-        <img class="small" src="./img/s`+i+`.jpg" alt="작은이미지${i}">
-        <img class="big" src="./img/pic${i}.jpg" alt="큰이미지${i}">
-        <h2 class="title">art work ${i}</h2>
-        <p>Artwork description comes here.<br>2023.06.20</p>
-        <span class="btnClose">close</span>
-    </li>`
+    // for(let i=1 ; i<=20 ; i++){
+    //     result+=`<li>
+    //     <img class="small" src="./img/s`+i+`.jpg" alt="작은이미지${i}">
+    //     <img class="big" src="./img/pic${i}.jpg" alt="큰이미지${i}">
+    //     <h2 class="title">art work ${i}</h2>
+    //     <p>Artwork description comes here.<br>2023.06.20</p>
+    //     <span class="btnClose">close</span>
+    // </li>`
+    // }
+
+    for(let i=0 ; i<20 ; i++){
+        
     }
+
     $(".train").html(result)
 
     $(".stationMenu>li").click(function(){
